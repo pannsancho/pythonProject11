e8329646da1185a881c364ad08b5cd3d5bc6a0d8
Aleksandr Antonov <antonoffa@list.ru>
2024-02-06 15:04:49 +0300

    Если есть - возвращаем, если отсутствует - выводим сообщение об ошибке

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ def get_file_extension(file_name):
     try:
         file_parts = file_name.split(".")
         if len(file_parts) > 1:
+            return file_parts[-1]
+        else:
+            raise ValueError("Файл не имеет расширения.")
+    except Exception as e:
+        print(f"Ошибка: {e}")
+
 
 def main():
     ...
+
 
 if __name__ == "__main__":
     main()
